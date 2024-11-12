e68e5ef058a91a0be671fb930319a65cd8f25e2e
joohygkim <37782507+joohygkim@users.noreply.github.com>
2024-11-11 21:18:07 -0500

    Update do-lab.sh

--- lab1/part1/do-lab.sh
+++ lab1/part1/do-lab.sh
@@ -6,3 +6,10 @@
 # run make aliases
 source ./provided/make_aliases.sh
 
+# Create bridge on switch
+sw ip link add name mybrdige type bridge
+sw ip link set mybrdige up
+sw ip link set eth1 master mybridge
+sw ip link set eth2 master mybridge
+sw ip link set eth3 master mybridge
+sw ip link set eth4 master mybridge
